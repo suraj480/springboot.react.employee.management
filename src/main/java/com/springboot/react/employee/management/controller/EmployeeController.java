27cb3417ee656b21c1aa9a594bf5fe9d34d14bbb
suraj480 <surajmaurya991@gmail.com>
2024-01-01 20:09:53 +0530

    update employee

--- src/main/java/com/springboot/react/employee/management/controller/EmployeeController.java
+++ src/main/java/com/springboot/react/employee/management/controller/EmployeeController.java
@@ -3,13 +3,21 @@ package com.springboot.react.employee.management.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.springboot.react.employee.management.exception.ResourceNotFoundException;
 import com.springboot.react.employee.management.model.Employee;
 import com.springboot.react.employee.management.repository.EmployeeRepository;
 
+@CrossOrigin(origins = "http://localhost:3000/")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
@@ -21,4 +29,30 @@ public class EmployeeController {
 	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
 	}
+
+//Create Employee Rest api
+	@PostMapping("/employees")
+	public Employee createEmplotee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
+	}
+
+//get employee by id rest api
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exists with id:" + id));
+		return ResponseEntity.ok(employee);
+	}
+
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not exists with id:" + id));
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmailId(employeeDetails.getEmailId());
+		Employee updatedEmployee = employeeRepository.save(employee);
+		return ResponseEntity.ok(updatedEmployee);
+	}
+
 }
